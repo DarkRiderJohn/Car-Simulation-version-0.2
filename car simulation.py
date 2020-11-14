a1b1bad1caaa7f9a747660ca391eaf21a6ff31f5
DarkRiderJohn <68285881+DarkRiderJohn@users.noreply.github.com>
2020-11-14 20:42:05 +0545

    Add files via upload

---
+++ car simulation.py
@@ -0,0 +1,63 @@
+from playsound import playsound
+'''
+This is the second improve version of car simulation
+'''
+print('Car version: 0.2')
+print('Since it is the second version it is the second  worst version')
+print()
+print()
+
+print('Well come to car simulation')
+print("if you are new here enter 'help'")
+start = False
+while True:
+    user_input = str.lower(input('>>'))
+
+    if user_input == 'help':
+        print('''
+commands:   uses
+help    :   shows the list of commands
+start   :   Starts the car engines
+run     :   runs the car
+horn    :   Cmmon you know it
+stop    :   stops the car only when car is running
+quit    :   closes the program
+    ''')
+    elif user_input == 'start' and start == True:
+        playsound('E:\ALL 2077\DarkRider\sounds\if car was started.mp3')
+
+    elif user_input == 'start':
+        playsound('E:\ALL 2077\DarkRider\sounds\carstartgarage.mp3')
+        start = True
+
+    elif user_input == 'run' and start != True:
+        print('atlest check whether engines has been started or not!')
+    elif user_input == 'run' and start == True:
+        playsound('E:\ALL 2077\DarkRider\sounds\Drive off squealing tires.wav')
+        print("Car started running but we don't have steering")
+
+    elif user_input == 'horn' and start != True:
+        print('atlest check whether engines has been started or not!')
+    elif user_input == 'horn' and start == True:
+        playsound('E:\ALL 2077\DarkRider\sounds\car+horn+x.mp3')
+
+
+    elif user_input == 'stop' and start == True:
+        playsound('E:\ALL 2077\DarkRider\sounds\car_brake_crash-Cam_Martinez.mp3')
+        print('Holyshit! close call')
+        start = False
+
+
+    elif user_input == 'quit' and start == True:
+        print("Can't quit when the engine is running")
+    elif user_input == 'quit' and start == False:
+        print('Okay exiting program now...')
+        break
+
+
+    else:
+        print("Invalid commands")
+
+print()
+print('New version will come soon even worst that this hahaha')
+print("Damn! enimnem rocks")
